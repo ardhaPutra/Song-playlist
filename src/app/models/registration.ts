d7de5ef781d232d54a31573a2740aa79f96349a1
Ardha_Putra <ardhakrs@gmail.com>
2020-04-13 11:49:30 +0800

    readme

--- src/app/models/registration.ts
+++ src/app/models/registration.ts
@@ -2,13 +2,11 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 export class Registration {
     constructor(
-      public firstName: string = "",
-      public lastName: string = "",
-      public dob: NgbDateStruct = null,
-      public doj: NgbDateStruct = null,
-      public email: string = "",
-      public password: string = "",
-      public company: string = "Select company"
+      public playlistName: string = "",
+      public songTitle: string = "",
+      public artist: string = "",
+      public description: string = "",
+      public duration: number = 0
     ) {}
   }
 
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,41 +12,34 @@ export class RegistrationComponent implements OnInit {
     showNew: Boolean = false;
     submitType: string = "Save";
     selectedRow: number;
-    companies: string[] = ["Company A", "Company B", "Company C", "Company D", "Company E"];
   constructor() {
       this.registrations.push(
       new Registration(
-        "Dave",
-        "Black",
-        { year: 1986, month: 1, day: 19 },
-        { year: 2008, month: 4, day: 1},
-        "daveblack@companya.com",
-        "pass123",
-        "Company A"
+        "90s Gangsta Rap",
+        "The Industy is Wack",
+        "Ultra",
+        "Most iconic gangsta rap from 90s",
+        4
       )
     );
 
     this.registrations.push(
       new Registration(
-        "Thomas",
-        "Tele",
-        { year: 1980, month: 4, day: 19 },
-        { year: 2004, month: 5, day: 1},
-        "thomastele@companyb.com",
-        "pass234",
-        "Company B"
+        "2000s Easycore Bands",
+        "Bada Bing Wit A Pipe",
+        "Four Year Strong",
+        "The Compilation of Best Easycore Band's tracks",
+        3.32
       )
     );
 
     this.registrations.push(
       new Registration(
-        "John",
-        "Richards",
-        { year: 1988, month: 7, day: 11 },
-        { year: 2010, month: 1, day: 3},
-        "johnrichards@companyc.com",
-        "pass345",
-        "Company C"
+        "Modern Metal",
+        "Abandoned",
+        "Unprocessed",
+        "Various songs from 2010s Metal Bands",
+        5.28
       )
     );
   }
@@ -65,13 +58,11 @@ onSave() {
   
   // Update existing 
   
-  this.registrations[this.selectedRow].firstName = this.regModel.firstName;  
-  this.registrations[this.selectedRow].lastName = this.regModel.lastName;  
-  this.registrations[this.selectedRow].dob = this.regModel.dob;
-  this.registrations[this.selectedRow].doj = this.regModel.doj;  
-  this.registrations[this.selectedRow].email = this.regModel.email;  
-  this.registrations[this.selectedRow].password = this.regModel.password;  
-  this.registrations[this.selectedRow].company = this.regModel.company;
+  this.registrations[this.selectedRow].playlistName = this.regModel.playlistName;  
+  this.registrations[this.selectedRow].songTitle = this.regModel.songTitle;  
+  this.registrations[this.selectedRow].artist = this.regModel.artist;    
+  this.registrations[this.selectedRow].description = this.regModel.description;  
+  this.registrations[this.selectedRow].duration = this.regModel.duration;    
 }
   
   this.showNew = false;
@@ -96,9 +87,6 @@ onCancel() {
   this.showNew = false;
 }
 
-onChangeCompany(company: string) {
-  this.regModel.company = company;
-}
   ngOnInit() {}
 }
 
